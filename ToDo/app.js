e611dd9763a6a90e14d15fa9e70934c194e78ee6
3mptySpac3 <thefantasticalproject@gmail.com>
2024-05-07 17:57:41 -0600

    local storage

--- ToDo/app.js
+++ ToDo/app.js
@@ -33,15 +33,30 @@ function addTask(){
     span.innerHTML = "\u00D7";
     li.appendChild(span);
   }
+  saveData();
 }
 
 listContainer.addEventListener('click', function(e) {
   if (e.target.tagName === 'LI') {
     e.target.classList.toggle('checked');
+    saveData
   } else if (e.target.tagName === 'SPAN') {
     e.target.parentElement.remove ();
+    saveData();
   }
-});
+}, false);
+
+function saveData() {
+  localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function loadData() {
+  listContainer.innerHTML = localStorage.getItem('data');
+}
+
+loadData(); // Load saved data on page load
+
+
 
 addButton.addEventListener('click', addTask);  
 
