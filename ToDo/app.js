79b342f3fdfc3cd810dd82cf5be58c4e8594335e
3mptySpac3 <thefantasticalproject@gmail.com>
2024-05-07 19:30:45 -0600

    fixes

--- ToDo/app.js
+++ ToDo/app.js
@@ -28,17 +28,16 @@ function updateCharCount() {
 
 function addTask(){
   if (inputBox.value === '') {
-    alert('Please enter a task');
   } else {
     let li = document.createElement('li');
     li.textContent = inputBox.value;  
     listContainer.appendChild(li);
-    inputBox.value = '';  
     let span = document.createElement('span');
     span.innerHTML = "\u00D7";
     li.appendChild(span);
     updateCharCount();
   }
+  inputBox.value = '';  
   saveData();
 }
 
